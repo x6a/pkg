06145e3d35bec1590f86446eb8db7287951e9fae
x6a <x6a@7n.io>
2019-05-16 00:07:13 +0200

    added preliminary version of errors pkg

---
+++ errors/errors.go
@@ -0,0 +1,75 @@
+// Copyright © 2019 <x6a@7n.io>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package errors
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+	"runtime"
+
+	"github.com/pkg/errors"
+)
+
+type ErrorStatus struct {
+	Status int    `json:"status"`
+	Msg    string `json:"msg"`
+}
+
+func New(msg string) error {
+	return errors.New(msg)
+}
+
+func Errorf(format string, args ...interface{}) error {
+	return errors.Errorf(format, args...)
+}
+
+func Wrapf(err error, format string, args ...interface{}) error {
+	return errors.Wrapf(err, format, args...)
+}
+
+func HandleFatalError(err error) {
+	if err != nil {
+		log.Fatalln("ERROR:", err)
+	}
+}
+
+func Trace() string {
+	pc := make([]uintptr, 10) // at least 1 entry needed
+	runtime.Callers(2, pc)
+	f := runtime.FuncForPC(pc[0])
+	file, line := f.FileLine(pc[0])
+	return fmt.Sprintf("%s:%d | %s", filepath.Base(file), line, f.Name())
+}
+
+func Trace2() string {
+	pc := make([]uintptr, 15)
+	n := runtime.Callers(2, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	frame, _ := frames.Next()
+	return fmt.Sprintf("%s:%d/%s", filepath.Base(frame.File), frame.Line, frame.Function)
+}
+
+/*
+{
+  "error": {
+    "message": "(#803) Some of the aliases you requested do not exist: products",
+    "type": "OAuthException",
+    "code": 803,
+    "fbtrace_id": "FOXX2AhLh80"
+  }
+}
+*/
