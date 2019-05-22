d72f4feef3a88f0fb3c2ecd0f72ffaac3d3b016c
x6a <x6a@7n.io>
2019-05-22 05:01:42 +0200

    added utils/string.go

---
+++ utils/strings.go
@@ -0,0 +1,44 @@
+// Copyright (C) 2019 <x6a@7n.io>
+//
+// pkg is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// pkg is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with pkg. If not, see <http://www.gnu.org/licenses/>.
+
+package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+// RandString generates a random ASCII string with at least one digit
+// and one special character.
+func RandString(length int) string {
+	rand.Seed(time.Now().UnixNano())
+
+	digits := "0123456789"
+	specials := "~=+%^*/()[]{}/!@#$?|"
+	all := "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+		"abcdefghijklmnopqrstuvwxyz" +
+		digits + specials
+	buf := make([]byte, length)
+	buf[0] = digits[rand.Intn(len(digits))]
+	buf[1] = specials[rand.Intn(len(specials))]
+	for i := 2; i < length; i++ {
+		buf[i] = all[rand.Intn(len(all))]
+	}
+	rand.Shuffle(len(buf), func(i, j int) {
+		buf[i], buf[j] = buf[j], buf[i]
+	})
+
+	return string(buf) // E.g. "3i[g0|)z"
+}
