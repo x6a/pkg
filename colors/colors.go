440547c6fe4935177cab3d9a6dae459741868a84
x6a <x6a@7n.io>
2020-01-05 00:16:16 +0100

    colors: added inverted colors

--- colors/colors.go
+++ colors/colors.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 <x6a@7n.io>
+// Copyright (C) 2019 x6a
 //
 // pkg is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -20,24 +20,32 @@ import (
 )
 
 var Black = ansi.ColorFunc("black+bh")
+var InvertedBlack = ansi.ColorFunc("white+bh:black")
 
 var White = ansi.ColorFunc("white+bh")
 var DarkWhite = ansi.ColorFunc("white+b")
+var InvertedWhite = ansi.ColorFunc("black+b:white+h")
 
 var Blue = ansi.ColorFunc("blue+bh")
 var DarkBlue = ansi.ColorFunc("blue+b")
+var InvertedBlue = ansi.ColorFunc("white+bh:blue")
 
 var Cyan = ansi.ColorFunc("cyan+bh")
 var DarkCyan = ansi.ColorFunc("cyan+b")
+var InvertedCyan = ansi.ColorFunc("white+bh:cyan")
 
 var Red = ansi.ColorFunc("red+bh")
 var DarkRed = ansi.ColorFunc("red+b")
+var InvertedRed = ansi.ColorFunc("white+bh:red")
 
-var Green = ansi.ColorFunc("gree+bh")
-var DarkGreen = ansi.ColorFunc("gree+b")
+var Green = ansi.ColorFunc("green+bh")
+var DarkGreen = ansi.ColorFunc("green+b")
+var InvertedGreen = ansi.ColorFunc("white+bh:green")
 
 var Magenta = ansi.ColorFunc("magenta+bh")
 var DarkMagenta = ansi.ColorFunc("magenta+b")
+var InvertedMagenta = ansi.ColorFunc("white+bh:magenta")
 
 var Yellow = ansi.ColorFunc("yellow+bh")
 var DarkYellow = ansi.ColorFunc("yellow+b")
+var InvertedYellow = ansi.ColorFunc("white+bh:yellow")
