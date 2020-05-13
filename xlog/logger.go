4c97cf27d1e0c25f0ea7fd0fd94bf5365eb07751
x6a <x6a@7n.io>
2020-05-14 01:11:31 +0200

    xlog: improvements

--- xlog/logger.go
+++ xlog/logger.go
@@ -155,6 +155,33 @@ func WithSlack(opt *SlackOption) *LogOption {
 	}
 }
 
+func GetLogLevel(loglevel string) LogLevel {
+	if strings.Contains(strings.ToUpper(loglevel), "TRACE") {
+		return TRACE
+	}
+	if strings.Contains(strings.ToUpper(loglevel), "DEBUG") {
+		return DEBUG
+	}
+	if strings.Contains(strings.ToUpper(loglevel), "INFO") {
+		return INFO
+	}
+	if strings.Contains(strings.ToUpper(loglevel), "WARN") {
+		return WARN
+	}
+	if strings.Contains(strings.ToUpper(loglevel), "ERROR") {
+		return ERROR
+	}
+	if strings.Contains(strings.ToUpper(loglevel), "ALERT") {
+		return ALERT
+	}
+
+	return -1
+}
+
+func (ll LogLevel) String() string {
+	return logPrefixes[ll]
+}
+
 func (l *logger) setOptions(logOpts ...*LogOption) {
 	for _, opt := range logOpts {
 		switch opt.key {
